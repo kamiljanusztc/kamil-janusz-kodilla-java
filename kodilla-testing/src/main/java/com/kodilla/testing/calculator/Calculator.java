ac7c6d3bbd4d550e0cda220b0d404b948008d7bc
Kamil Janusz <kamiljanusztc@gmail.com>
2022-04-06 14:08:03 +0200

    Add Calculator class with test

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,17 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    private int a;
+    private int b;
+
+    public static int addNumbers(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+    public static int subtractNumbers(int a, int b) {
+        int sum = a - b;
+        return sum;
+    }
+}
