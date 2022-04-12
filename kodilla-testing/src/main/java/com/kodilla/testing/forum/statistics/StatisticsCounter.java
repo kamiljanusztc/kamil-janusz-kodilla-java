7de0c1fa71af904f644feee6dfaf46714c549fa0
Kamil Janusz <kamiljanusztc@gmail.com>
2022-04-12 16:45:05 +0200

    Add structure for stream module

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/StatisticsCounter.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/StatisticsCounter.java
@@ -1,43 +1,43 @@
-package com.kodilla.testing.forum.statistics;
-
-import com.kodilla.testing.forum.ForumComment;
-import com.kodilla.testing.forum.ForumPost;
-
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-
-public class StatisticsCounter implements Statistics {
-
-    Statistics statistics;
-
-    public void calculateAdvStatistics(Statistics statistics) {
-
-//        liczbę użytkowników,
-//        liczbę postów,
-//        liczbę komentarzy,
-//        średnią liczbę postów na użytkownika,
-//        średnią liczbę komentarzy na użytkownika,
-//        średnią liczbę komentarzy na post.
-//        która obliczy podane powyżej wartości i zapamięta je we właściwościach (polach) klasy.
-    }
-
-    public void showStatistics() {
-//        wyświetli zapamiętane we właściwościach statystyki.
-    }
-
-    @Override
-    public List<String> usersNames() {
-        return null;
-    }
-
-    @Override
-    public int postsCount() {
-        return 0;
-    }
-
-    @Override
-    public int commentsCount() {
-        return 0;
-    }
-}
+//package com.kodilla.testing.forum.statistics;
+//
+//import com.kodilla.testing.forum.ForumComment;
+//import com.kodilla.testing.forum.ForumPost;
+//
+//import java.util.ArrayList;
+//import java.util.LinkedList;
+//import java.util.List;
+//
+//public class StatisticsCounter implements Statistics {
+//
+//    Statistics statistics;
+//
+//    public void calculateAdvStatistics(Statistics statistics) {
+//
+////        liczbę użytkowników,
+////        liczbę postów,
+////        liczbę komentarzy,
+////        średnią liczbę postów na użytkownika,
+////        średnią liczbę komentarzy na użytkownika,
+////        średnią liczbę komentarzy na post.
+////        która obliczy podane powyżej wartości i zapamięta je we właściwościach (polach) klasy.
+//    }
+//
+//    public void showStatistics() {
+////        wyświetli zapamiętane we właściwościach statystyki.
+//    }
+//
+//    @Override
+//    public List<String> usersNames() {
+//        return null;
+//    }
+//
+//    @Override
+//    public int postsCount() {
+//        return 0;
+//    }
+//
+//    @Override
+//    public int commentsCount() {
+//        return 0;
+//    }
+//}
--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsTestSuite.java
@@ -1,97 +1,97 @@
-package com.kodilla.testing.forum.statistics;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.mockito.Mockito.when;
-
-@ExtendWith(MockitoExtension.class)
-class StatisticsTestSuite {
-
-    private static int testCounter = 0;
-
-    @Mock
-    private Statistics statisticsMock;
-
-    @BeforeEach
-    public void beforeEveryTest() {
-        testCounter++;
-        System.out.println("Preparing to execute test #" + testCounter);
-    }
-
-    @Nested
-    @DisplayName("Tests for posts")
-    class TestPosts {
-
-        @Test
-        void testPostsEqual0() {
-            // Given
-            StatisticsCounter statisticsCounter = new StatisticsCounter();
-            when(statisticsMock.postsCount()).thenReturn(0);
-            statisticsCounter.calculateAdvStatistics(statisticsMock);
-
-            // When
-            
-
-            // Then
-        }
-
-        @Test
-        void testPostsEqual1000() {
-            // Given
-            // When
-            // Then
-        }
-    }
-
-    @Nested
-    @DisplayName("Tests for comments")
-    class TestComments {
-
-        @Test
-        void testCommentsEqual0() {
-            // Given
-            // When
-            // Then
-        }
-
-        @Test
-        void testCommentsLessThanPosts() {
-            // Given
-            // When
-            // Then
-        }
-
-        @Test
-        void testCommentsMoreThanPosts() {
-            // Given
-            // When
-            // Then
-        }
-    }
-
-    @Nested
-    @DisplayName("Tests for users")
-    class TestUsers {
-
-        @Test
-        void testUsersEqual0() {
-            // Given
-            // When
-            // Then
-        }
-
-        @Test
-        void testUsersEqual100() {
-            // Given
-            // When
-            // Then
-        }
-    }
-}
-
+//package com.kodilla.testing.forum.statistics;
+//
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Nested;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.Mock;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//
+//import static org.mockito.Mockito.when;
+//
+//@ExtendWith(MockitoExtension.class)
+//class StatisticsTestSuite {
+//
+//    private static int testCounter = 0;
+//
+//    @Mock
+//    private Statistics statisticsMock;
+//
+//    @BeforeEach
+//    public void beforeEveryTest() {
+//        testCounter++;
+//        System.out.println("Preparing to execute test #" + testCounter);
+//    }
+//
+//    @Nested
+//    @DisplayName("Tests for posts")
+//    class TestPosts {
+//
+//        @Test
+//        void testPostsEqual0() {
+//            // Given
+//            StatisticsCounter statisticsCounter = new StatisticsCounter();
+//            when(statisticsMock.postsCount()).thenReturn(0);
+//            statisticsCounter.calculateAdvStatistics(statisticsMock);
+//
+//            // When
+//
+//
+//            // Then
+//        }
+//
+//        @Test
+//        void testPostsEqual1000() {
+//            // Given
+//            // When
+//            // Then
+//        }
+//    }
+//
+//    @Nested
+//    @DisplayName("Tests for comments")
+//    class TestComments {
+//
+//        @Test
+//        void testCommentsEqual0() {
+//            // Given
+//            // When
+//            // Then
+//        }
+//
+//        @Test
+//        void testCommentsLessThanPosts() {
+//            // Given
+//            // When
+//            // Then
+//        }
+//
+//        @Test
+//        void testCommentsMoreThanPosts() {
+//            // Given
+//            // When
+//            // Then
+//        }
+//    }
+//
+//    @Nested
+//    @DisplayName("Tests for users")
+//    class TestUsers {
+//
+//        @Test
+//        void testUsersEqual0() {
+//            // Given
+//            // When
+//            // Then
+//        }
+//
+//        @Test
+//        void testUsersEqual100() {
+//            // Given
+//            // When
+//            // Then
+//        }
+//    }
+//}
+//
