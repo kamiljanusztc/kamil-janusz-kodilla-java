52b364255444e2c17b8db3d9017f6a4beef9a961
Kamil Janusz <kamiljanusztc@gmail.com>
2022-04-13 19:42:46 +0200

    Add Immutable ForumUser class

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,14 +1,9 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
-
-import java.util.Locale;
-
-import static com.sun.tools.javac.util.StringUtils.toUpperCase;
-import static jdk.javadoc.internal.doclets.toolkit.util.Utils.toLowerCase;
 
 public class StreamMain {
 
@@ -30,10 +25,13 @@ public class StreamMain {
         PoemBeautifier poemBeautifier = new PoemBeautifier();
 
         System.out.println("Adding a text decorator");
-        poemBeautifier.beautify("Swimming", (text) -> toLowerCase(text));
-        poemBeautifier.beautify("Swimming", (text) -> toUpperCase(text));
+        poemBeautifier.beautify("Swimming", (text) -> text.toLowerCase());
+        poemBeautifier.beautify("Swimming", (text) -> text.toUpperCase());
         poemBeautifier.beautify("Swimming", (text) -> "ABC " + text);
         poemBeautifier.beautify("Swimming", (text) -> "/n" + text + "/n");
         poemBeautifier.beautify("Swimming", (text) -> text + " ABC");
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
