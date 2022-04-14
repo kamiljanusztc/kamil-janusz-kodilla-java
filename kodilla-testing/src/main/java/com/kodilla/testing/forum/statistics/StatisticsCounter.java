600fc163e4395c5d721b1366a50b55e006d5f212
Kamil Janusz <kamiljanusztc@gmail.com>
2022-04-14 21:51:18 +0200

    Add Statistics tests

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/StatisticsCounter.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/StatisticsCounter.java
@@ -1,43 +1,54 @@
-//package com.kodilla.testing.forum.statistics;
-//
-//import com.kodilla.testing.forum.ForumComment;
-//import com.kodilla.testing.forum.ForumPost;
-//
-//import java.util.ArrayList;
-//import java.util.LinkedList;
-//import java.util.List;
-//
-//public class StatisticsCounter implements Statistics {
-//
-//    Statistics statistics;
-//
-//    public void calculateAdvStatistics(Statistics statistics) {
-//
-////        liczbę użytkowników,
-////        liczbę postów,
-////        liczbę komentarzy,
-////        średnią liczbę postów na użytkownika,
-////        średnią liczbę komentarzy na użytkownika,
-////        średnią liczbę komentarzy na post.
-////        która obliczy podane powyżej wartości i zapamięta je we właściwościach (polach) klasy.
-//    }
-//
-//    public void showStatistics() {
-////        wyświetli zapamiętane we właściwościach statystyki.
-//    }
-//
-//    @Override
-//    public List<String> usersNames() {
-//        return null;
-//    }
-//
-//    @Override
-//    public int postsCount() {
-//        return 0;
-//    }
-//
-//    @Override
-//    public int commentsCount() {
-//        return 0;
-//    }
-//}
+package com.kodilla.testing.forum.statistics;
+
+public class StatisticsCounter {
+
+    private int userCount;
+    private int postsCount;
+    private int commentsCount;
+    private double averagePostsPerUser;
+    private double averageCommentsPerUser;
+    private double averageCommentsPerPosts;
+
+    public void calculateAdvStatistics(Statistics statistics) {
+
+        userCount = statistics.usersNames().size();
+        postsCount = statistics.postsCount();
+        commentsCount = statistics.commentsCount();
+        averagePostsPerUser = (double)postsCount / userCount;
+        averageCommentsPerUser = (double)commentsCount / userCount;
+        averageCommentsPerPosts = (double)commentsCount / postsCount;
+    }
+
+    public int getUserCount() {
+        return userCount;
+    }
+
+    public int getPostsCount() {
+        return postsCount;
+    }
+
+    public int getCommentsCount() {
+        return commentsCount;
+    }
+
+    public double getAveragePostsPerUser() {
+        return averagePostsPerUser;
+    }
+
+    public double getAverageCommentsPerUser() {
+        return averageCommentsPerUser;
+    }
+
+    public double getAverageCommentsPerPosts() {
+        return averageCommentsPerPosts;
+    }
+
+    public void showStatistics() {
+        System.out.println("Users: " + userCount);
+        System.out.println("Posts: " + userCount);
+        System.out.println("Comments: " + userCount);
+        System.out.println("Average posts per user : " + averagePostsPerUser);
+        System.out.println("Average comments per user : " + averageCommentsPerUser);
+        System.out.println("Average comments per posts : " + averageCommentsPerPosts);
+    }
+}
--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsTestSuite.java
@@ -1,97 +1,164 @@
-//package com.kodilla.testing.forum.statistics;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Nested;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//
-//import static org.mockito.Mockito.when;
-//
-//@ExtendWith(MockitoExtension.class)
-//class StatisticsTestSuite {
-//
-//    private static int testCounter = 0;
-//
-//    @Mock
-//    private Statistics statisticsMock;
-//
-//    @BeforeEach
-//    public void beforeEveryTest() {
-//        testCounter++;
-//        System.out.println("Preparing to execute test #" + testCounter);
-//    }
-//
-//    @Nested
-//    @DisplayName("Tests for posts")
-//    class TestPosts {
-//
-//        @Test
-//        void testPostsEqual0() {
-//            // Given
-//            StatisticsCounter statisticsCounter = new StatisticsCounter();
-//            when(statisticsMock.postsCount()).thenReturn(0);
-//            statisticsCounter.calculateAdvStatistics(statisticsMock);
-//
-//            // When
-//
-//
-//            // Then
-//        }
-//
-//        @Test
-//        void testPostsEqual1000() {
-//            // Given
-//            // When
-//            // Then
-//        }
-//    }
-//
-//    @Nested
-//    @DisplayName("Tests for comments")
-//    class TestComments {
-//
-//        @Test
-//        void testCommentsEqual0() {
-//            // Given
-//            // When
-//            // Then
-//        }
-//
-//        @Test
-//        void testCommentsLessThanPosts() {
-//            // Given
-//            // When
-//            // Then
-//        }
-//
-//        @Test
-//        void testCommentsMoreThanPosts() {
-//            // Given
-//            // When
-//            // Then
-//        }
-//    }
-//
-//    @Nested
-//    @DisplayName("Tests for users")
-//    class TestUsers {
-//
-//        @Test
-//        void testUsersEqual0() {
-//            // Given
-//            // When
-//            // Then
-//        }
-//
-//        @Test
-//        void testUsersEqual100() {
-//            // Given
-//            // When
-//            // Then
-//        }
-//    }
-//}
-//
+package com.kodilla.testing.forum.statistics;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class StatisticsTestSuite {
+
+    private static int testCounter = 0;
+
+    @Mock
+    private Statistics statisticsMock;
+
+    @BeforeEach
+    public void beforeEveryTest() {
+        testCounter++;
+        System.out.println("Preparing to execute test #" + testCounter);
+    }
+
+    @Nested
+    @DisplayName("Tests for posts")
+    class TestPosts {
+
+        @Test
+        void testPostsEqual0() {
+            // Given
+            StatisticsCounter statisticsCounter = new StatisticsCounter();
+            when(statisticsMock.postsCount()).thenReturn(0);
+            when(statisticsMock.usersNames()).thenReturn(List.of("Adam", "Marek"));
+            when(statisticsMock.commentsCount()).thenReturn(10);
+
+            // When
+            statisticsCounter.calculateAdvStatistics(statisticsMock);
+
+            // Then
+            assertEquals(0, statisticsCounter.getPostsCount());
+            assertEquals(10, statisticsCounter.getCommentsCount());
+            assertEquals(2, statisticsCounter.getUserCount());
+        }
+
+        @Test
+        void testPostsEqual1000() {
+            // Given
+            StatisticsCounter statisticsCounter = new StatisticsCounter();
+            when(statisticsMock.postsCount()).thenReturn(1000);
+            when(statisticsMock.usersNames()).thenReturn(List.of("Joe", "Anna"));
+            when(statisticsMock.commentsCount()).thenReturn(3);
+
+            // When
+            statisticsCounter.calculateAdvStatistics(statisticsMock);
+
+            // Then
+            assertEquals(1000, statisticsCounter.getPostsCount());
+        }
+    }
+
+    @Nested
+    @DisplayName("Tests for comments")
+    class TestComments {
+
+        @Test
+        void testCommentsEqual0() {
+            // Given
+            StatisticsCounter statisticsCounter = new StatisticsCounter();
+            when(statisticsMock.postsCount()).thenReturn(1000);
+            when(statisticsMock.usersNames()).thenReturn(List.of("Joe", "Anna"));
+            when(statisticsMock.commentsCount()).thenReturn(0);
+
+            // When
+            statisticsCounter.calculateAdvStatistics(statisticsMock);
+
+            // Then
+            assertEquals(0, statisticsCounter.getCommentsCount());
+            assertEquals(1000, statisticsCounter.getPostsCount());
+            assertEquals(2, statisticsCounter.getUserCount());
+        }
+
+        @Test
+        void testCommentsLessThanPosts() {
+            // Given
+            StatisticsCounter statisticsCounter = new StatisticsCounter();
+            when(statisticsMock.postsCount()).thenReturn(23);
+            when(statisticsMock.usersNames()).thenReturn(List.of("Joe", "Anna"));
+            when(statisticsMock.commentsCount()).thenReturn(2);
+
+            // When
+            statisticsCounter.calculateAdvStatistics(statisticsMock);
+
+            // Then
+            assertTrue(statisticsCounter.getCommentsCount() < statisticsCounter.getPostsCount());
+            assertEquals(2, statisticsCounter.getUserCount());
+        }
+
+        @Test
+        void testCommentsMoreThanPosts() {
+            // Given
+            StatisticsCounter statisticsCounter = new StatisticsCounter();
+            when(statisticsMock.postsCount()).thenReturn(10);
+            when(statisticsMock.usersNames()).thenReturn(List.of("Joe", "Anna"));
+            when(statisticsMock.commentsCount()).thenReturn(232);
+
+            // When
+            statisticsCounter.calculateAdvStatistics(statisticsMock);
+
+            // Then
+            assertTrue(statisticsCounter.getCommentsCount() > statisticsCounter.getPostsCount());
+            assertEquals(2, statisticsCounter.getUserCount());
+        }
+    }
+
+    @Nested
+    @DisplayName("Tests for users")
+    class TestUsers {
+
+        @Test
+        void testUsersEqual0() {
+            // Given
+            StatisticsCounter statisticsCounter = new StatisticsCounter();
+            List<String> userList = new ArrayList<>();
+            when(statisticsMock.usersNames()).thenReturn(userList);
+            when(statisticsMock.postsCount()).thenReturn(0);
+            when(statisticsMock.commentsCount()).thenReturn(0);
+
+            // When
+            statisticsCounter.calculateAdvStatistics(statisticsMock);
+
+            // Then
+            assertEquals(0, statisticsCounter.getPostsCount());
+            assertEquals(0, statisticsCounter.getCommentsCount());
+            assertEquals(statisticsCounter.getUserCount(), userList.size());
+        }
+
+        @Test
+        void testUsersEqual100() {
+            // Given
+            StatisticsCounter statisticsCounter = new StatisticsCounter();
+            List<String> userList = new ArrayList<>();
+            for (int i = 0; i < 100; i++) {
+                userList.add("Person");
+            }
+            when(statisticsMock.usersNames()).thenReturn(userList);
+            when(statisticsMock.postsCount()).thenReturn(1000);
+            when(statisticsMock.commentsCount()).thenReturn(3);
+
+            // When
+            statisticsCounter.calculateAdvStatistics(statisticsMock);
+
+            // Then
+            assertEquals(100, statisticsCounter.getUserCount());
+            assertEquals(3, statisticsCounter.getCommentsCount());
+            assertEquals(1000, statisticsCounter.getPostsCount());
+        }
+    }
+}
+
