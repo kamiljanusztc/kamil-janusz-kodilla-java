c7c5de6b642910808e048104128d9faae9c1c027
Kamil Janusz <kamiljanusztc@gmail.com>
2022-04-16 11:36:36 +0200

    Remove old imports

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -1,10 +1,6 @@
 package com.kodilla.stream.array;
 
 import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
-import java.util.OptionalDouble;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ArrayOperationsTestSuite {
