522622072311cdff40ba6d7dfb9bcccba2b076c0
Kamil Janusz <kamiljanusztc@gmail.com>
2022-04-21 15:25:21 +0200

    Add Exception test

---
+++ kodilla-exception/src/test/java/com/kodilla/exception/test/ThrowExceptionTestSuite.java
@@ -0,0 +1,19 @@
+package com.kodilla.exception.test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ThrowExceptionTestSuite {
+
+    @Test
+    void testThrowException() {
+        //Given
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        //When & Then
+        assertAll(
+                () -> assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(2.0, 1.5)),
+                () -> assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(1.1, 2.1)),
+                () -> assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(1.1, 1.1))
+        );
+    }
+}
