6fa0067811d04ec07858353d4b2120afc2862403
Kamil Janusz <kamiljanusztc@gmail.com>
2022-06-07 19:00:47 +0200

    Add Logger test suite

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,22 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoggerTestSuite {
+
+    Logger logger = Logger.INSTANCE;
+
+    @Test
+    void testLastLog() {
+        //Given
+       logger.log("last log");
+//        logger = logger.getLastLog();
+        //When
+        String result = logger.getLastLog();
+        //Then
+        assertEquals("last log", result);
+    }
+}
